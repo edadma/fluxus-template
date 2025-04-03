a59268e2882f5b93eea9134a90bd7e4e932ea1fb
Edward A Maxedon <edadma@gmail.com>
2025-04-03 18:29:47 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
   plugins: [scalaJSPlugin()],
-  build: {
-    // Make sure Vite picks up your CSS file
-    assetsInlineLimit: 0
-  }
 });
